04f83abc7e03cf3cdcaacc718c271fe1153d45f6
someresources <junkster55555@gmail.com>
2020-09-20 02:32:42 -0400

    added background play script

--- scripts/play.sh
+++ scripts/play.sh
@@ -1,10 +1,21 @@
 #!/bin/bash 
 #if [ `whoami` = *j* ]; then
+
 #curl -curl -X PUT -d '{"cmd":"jason"}' https://someresources-55555.firebaseio.com/.json
 #fi
-open /Users/`whoami`/Desktop/Present/Sol/Strawberry*.m4a
-for i in $(seq 0 16); do
-#osascript -e "set volume $i"
-osascript -e "tell application \"iTunes\" to set sound volume to $i";
-sleep .25
+
+open /Users/`whoami`/Desktop/Present/Sol/Strawberry*.m4a --hide
+
+#set general volume
+osascript -e "set volume 3"
+
+for i in {1..15}; do
+	volume=`echo "$(( i / 10)).$(( i % 10))"`
+
+	#set itunes volume
+	osascript -e "tell application \"Music\" to set sound volume to $volume"
+	sleep .25
 done
+
+
+
