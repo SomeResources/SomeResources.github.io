0ed5a3622e053283d76ffce2234802ad8ff05cf1
someresources <junkster55555@gmail.com>
2020-09-20 01:07:49 -0400

    adding script

--- scripts/play.sh
+++ scripts/play.sh
@@ -3,3 +3,8 @@
 #curl -curl -X PUT -d '{"cmd":"jason"}' https://someresources-55555.firebaseio.com/.json
 #fi
 open /Users/`whoami`/Desktop/Present/Sol/Strawberry*.m4a
+for i in $(seq 0 16); do
+#osascript -e "set volume $i"
+osascript -e "tell application \"iTunes\" to set sound volume to $i";
+sleep .25
+done
