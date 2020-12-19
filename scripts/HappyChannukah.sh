a63ec8c9eccedf2ad079858c24cc7d62fe49cdf0
someresources <junkster55555@gmail.com>
2020-12-19 16:35:09 -0500

    added

--- scripts/HappyChannukah.sh
+++ scripts/HappyChannukah.sh
@@ -1,6 +1,15 @@
+rm happyChannukah.sh
+mkdir happyChannukah
+cd happyChannukah
 curl https://media3.giphy.com/media/3o6ZtkEchxeIh8mad2/giphy.gif --output happy.gif 
 sleep 1
 xdg-open happy.gif
 sleep 1
 echo "Happy Channukah!!!" > HappyChannukah
 xdg-open HappyChannukah
+echo press any key to exit...
+read -n1 ok
+cd ..
+rm -r happyChannukah
+
+
