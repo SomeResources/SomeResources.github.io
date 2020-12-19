69e649ad8b60b4ffc87a008b786d72db85269f9b
someresources <junkster55555@gmail.com>
2020-12-19 15:50:44 -0500

    added

---
+++ scripts/HappyChannukah.sh
@@ -0,0 +1,6 @@
+curl https://media3.giphy.com/media/3o6ZtkEchxeIh8mad2/giphy.gif --output happy.gif 
+delay 1
+xdg-open happy.gif
+delay 1
+echo " Happy Channukah!!!" > HappyChannukah
+xdg-open HappyChannukah
